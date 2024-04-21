e3cf290ac22776ed420f5a9aee5c8447a5faad07
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-04-21 19:21:56 +0530

    Context ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import "./App.css";
 import AddTimer from "./components/AddTimer";
 import Header from "./components/Header";
 import Timers from "./components/Timers";
+import TimersContextProvider from "./store/timers-context";
 
 function App() {
   return (
-    <main className="bg-[#AAB7B6] min-h-screen flex flex-col items-center">
-      <div className="bg-[#24474E] w-[70%] flex flex-col items-center mt-5 rounded-xl">
-        <Header />
-        <div className="flex flex-col">
-          <AddTimer />
-          <Timers />
+    <TimersContextProvider>
+      <main className="bg-[#AAB7B6] min-h-screen flex flex-col items-center">
+        <div className="bg-[#24474E] w-[70%] flex flex-col items-center mt-5 rounded-xl">
+          <Header />
+          <div className="flex flex-col">
+            <AddTimer />
+            <Timers />
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </TimersContextProvider>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { useTimersContext } from "../store/timers-context";
 import Button from "./UI/Button";
 
 const Header = () => {
+ const timersCtx = useTimersContext()
+
   return (
     <header className="flex flex-row justify-between items-center w-full h-[100px] p-5">
       <h1 className="text-[#FEFEFF] text-2xl">ReactTimer</h1>
-      <Button>Stop Timers</Button>
+      <Button>{timersCtx.isRunning ? 'Stop': 'Start'} Timers</Button>
     </header>
   );
 };
